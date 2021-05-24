ff9369b1a0183062b0cb0e998e783a45c1d24974
Sparrowhawk120 <67168819+Sparrowhawk120@users.noreply.github.com>
2021-05-23 20:15:23 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+window.onload=function() {
+	var answerSpan = document.getElementById('answer');
+	var answerMathField = MQ.MathField(answerSpan, {
+		autoCommands: 'pi Pi theta sqrt infty alpha beta gamma Gamma phi Phi mu nthroot int iint partial sum',
+		autoOperatorNames: 'sin cos tan arcsin arccos arctan log ln max min lim',
+		sumStartsWithNEquals: true,
+		supSubsRequireOperand: true,
+		handlers: {
+		  edit: function() {
+			var enteredMath = answerMathField.latex(); // Get entered math in LaTeX format
+			document.getElementById('equation').src = "https://latex.codecogs.com/png.image?" + enteredMath;
+			document.getElementById('equation').style.marginTop = "15px";
+			chrome.storage.local.set({'LaTeX': enteredMath}, function() {
+				
+			});
+		  },
+		  enter: function() {
+			 chrome.storage.local.get(['LaTeX'], function(result) {
+				answerMathField.focus();
+				answerMathField.keystroke('End Shift-Home Del');
+				
+				answerMathField.write(result.LaTeX);
+			});
+		  }
+		}
+	});
+}
